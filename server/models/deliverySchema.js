afd201ba3bc38b6f3ff87cf7ac9edca0a56a48d6
LuKz <kabombr@gmail.com>
2024-11-14 10:47:22 -0500

    a

---
+++ server/models/deliverySchema.js
@@ -0,0 +1,5 @@
+import mongoose, { Schema } from "mongoose";
+
+const deliverySchema = new mongoose.Schema({
+
+})
--- server/models/productSchema.js
+++ server/models/productSchema.js
@@ -1,27 +1,30 @@
 import mongoose, { Schema } from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    productName: {
-        type: String,
-        required: true
-    },
-    productPrice: {
-        type: Number,
-        required: true
-    },
-    productCategory: {
-        type: String,
-        required: true
-    },
-    productImage: {
-        type: String // Array to store multiple images
-    },
-    productDescription: {
-        type: String,
-        required: true
-    },
-    productQuantityInStock: {
-        type: Number
-    },
+  productName: {
+    type: String,
+    required: true,
+  },
+  productPrice: {
+    type: Number,
+    required: true,
+  },
+  productCategory: {
+    type: String,
+    required: true,
+  },
+  productImage: {
+    type: String, // Array to store multiple images
+  },
+  productDescription: {
+    type: String,
+    required: true,
+  },
+  productQuantity: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
 
-})
+export const Product = mongoose.model("Product", productSchema)
