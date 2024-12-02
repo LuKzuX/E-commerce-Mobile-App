063bea57202d0cc98511e3aea0e268bd822429c2
LuKz <kabombr@gmail.com>
2024-12-02 06:57:54 -0500

    a

--- server/app.js
+++ server/app.js
@@ -2,10 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 const app = express();
+import cors from "cors"
 import { connect } from "./db/connection.js";
 import { router } from "./routes/routes.js";
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(`/material-delivery`, router);
