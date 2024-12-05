809d359a28003869b3f2487bb690df6453e5473c
Lucas <kabombr@gmail.com>
2024-12-04 20:57:52 -0500

    a

--- server/controllers/cartControllers.js
+++ server/controllers/cartControllers.js
@@ -1,27 +1,37 @@
-import {User} from "../models/userSchema.js"
-import {Product} from "../models/productSchema.js"
+import { User } from "../models/userSchema.js";
+import { Product } from "../models/productSchema.js";
+import {
+  addMoreOfTheSameProductToCart,
+  addSpecificProductQuantityToCart,
+} from "../utils/cartUtils.js";
 
 export const getCartProducts = async (req, res, next) => {
-  const {_id} = req.user.user
-    try {
-      const loggedUser = await User.findById({_id})
-      res.send(loggedUser.cart)
-    } catch (error) {
-      res.send(error);
-    }
-  };
-
-export const addProductToCart = async(req, res, next) => {
-  const {id} = req.params
-  const {_id} = req.user.user
-  const {productQuantity} = req.body || 1
+  const { _id } = req.user.user;
   try {
-    const product = await Product.findById({_id: id})
-    const loggedUser = await User.findById({_id})
-    loggedUser.cart.push(product)
-    loggedUser.save()
-    res.send(loggedUser)
+    const loggedUser = await User.findById({ _id });
+    res.send(loggedUser.cart);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-}
+};
+
+export const addProductToCart = async (req, res, next) => {
+  const { id } = req.params;
+  const { _id } = req.user.user;
+  const { productQuantity } = req.body || 1;
+  try {
+    const product = await Product.findById({ _id: id });
+    const loggedUser = await User.findById({ _id });
+
+    if (productQuantity > 1) {
+      addSpecificProductQuantityToCart(loggedUser, id, productQuantity, product);
+    } else {
+      addMoreOfTheSameProductToCart(loggedUser, id, product);
+    }
+
+    loggedUser.save();
+    res.send(loggedUser);
+  } catch (error) {
+    res.send(error);
+  }
+};
---
+++ server/utils/cartUtils.js
@@ -0,0 +1,36 @@
+export const addMoreOfTheSameProductToCart = (user, id, productToBeAdded) => {
+  for (let i = 0; i < user.cart.length; i++) {
+    if (id == user.cart[i]._id.toString()) {
+      user.cart[i].quantity += 1;
+    } else {
+      user.cart.push(productToBeAdded);
+    }
+  }
+};
+
+export const addSpecificProductQuantityToCart = (
+  user,
+  id,
+  quantity,
+  product
+) => {
+  let found = false;
+  let x = null;
+  for (let i = 0; i < user.cart.length; i++) {
+    if (id == user.cart[i]._id.toString()) {
+      user.cart[i].quantity = quantity;
+      found = true;
+    } else {
+      if (!user.cart.length) {
+        x = 0;
+      } else {
+        x = user.cart.length - 1;
+      }
+    }
+  }
+  if (!found) {
+    console.log(x);
+    user.cart.push(product);
+    user.cart[0].quantity = quantity;
+  }
+};
