befd8d70783f25a64b492a2bc61528419f6e79e1
LuKz <kabombr@gmail.com>
2024-12-05 15:22:44 -0500

    a

--- server/utils/cartUtils.js
+++ server/utils/cartUtils.js
@@ -38,7 +38,10 @@ export const calculatePrice = async (user) => {
   const productIds = user.cart.map((obj) => obj._id.toString());
   const products = await Product.find({ _id: { $in: productIds } });
   for (let i = 0; i < user.cart.length; i++) {
-    user.cart[i].totalPrice = products[0].productPrice * user.cart[i].quantity;
+    user.cart[i].totalPrice = 0
+    user.cart[i].totalPrice = products[i].productPrice * user.cart[i].quantity;
   }
+  console.log(products);
+  
   await user.save();
 };
