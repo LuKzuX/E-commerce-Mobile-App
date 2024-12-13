aa7aaad9d769bb9408711697cdd6bb5047306fc6
LuKz <kabombr@gmail.com>
2024-12-13 15:47:19 -0500

    a

---
+++ client/app/test.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import axios from 'axios'
+
+export default function DetailsScreen() {
+  const [data, setData] = useState('')
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const products = await axios.get(
+          'http://localhost:5000/material-delivery/'
+        )
+        setData(products.data)
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text>test</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
