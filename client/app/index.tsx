9fb9d4bfd0ae88e017d07c8a06d9d78d492b1e60
Lucas <kabombr@gmail.com>
2024-12-13 07:32:43 -0500

    A

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.title}>GATINHO.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 100,
+    color: 'red'
+  }
+})
