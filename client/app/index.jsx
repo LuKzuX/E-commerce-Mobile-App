4adcf3c881aa74f165c10abe9b45ce95fc9b272d
LuKz <kabombr@gmail.com>
2024-12-21 18:39:33 -0500

    a

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'expo-router'
+import { ip } from '../getIp.js'
 import {
   View,
   Text,
@@ -7,26 +7,27 @@ import {
   Image,
   FlatList,
   Platform,
+  TextInput,
 } from 'react-native'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigation } from '@react-navigation/native'
 import * as ImagePicker from 'expo-image-picker'
 import * as FileSystem from 'expo-file-system'
-import { uploadImage } from '../utils/uploadImage'
+import { uploadData } from '../utils/uploadImage.js'
 
 export default function HomeScreen() {
   const navigation = useNavigation()
   const [data, setData] = useState('')
-  const [productName, setProductName] = useState("")
-  const [productPrice, setProductPrice] = useState("")
-  const [productCategory, setProductCategory] = useState("")
-  const [productDescription, setProductDescription] = useState("")
-  const [productQuantity, setProductQuantity] = useState("")
+  const [productName, setProductName] = useState('')
+  const [productPrice, setProductPrice] = useState('')
+  const [productCategory, setProductCategory] = useState('')
+  const [productDescription, setProductDescription] = useState('')
+  const [productQuantity, setProductQuantity] = useState('')
 
   const getData = async () => {
     try {
-      const products = await axios.get('http://192.168.1.111:5000/material-delivery/')
+      const products = await axios.get(`http://${ip}:5000/material-delivery/`)
       setData(products.data)
       console.log(products.data)
     } catch (error) {
@@ -40,21 +41,56 @@ export default function HomeScreen() {
 
   return (
     <>
-      <Text style={styles.title}>Image Uploader</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setProductName(text)}
+        placeholder='name'
+        keyboardType='letter'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setProductPrice(text)}
+        placeholder='price'
+        keyboardType='numeric'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setProductCategory(text)}
+        placeholder='category'
+        keyboardType='letter'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setProductDescription(text)}
+        placeholder='desc'
+        keyboardType='letter'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setProductQuantity(text)}
+        placeholder='quantity'
+        keyboardType='numeric'
+      />
+
       <Button
         title='Select Image'
         onPress={async () => {
-          await uploadImage(
-            'http://192.168.1.111:5000/material-delivery/new-product'
-          ), getData()
+          await uploadData(`http://${ip}:5000/material-delivery/new-product`,
+            productName,
+            productPrice,
+            productCategory,
+            productDescription,
+            productQuantity,
+          )
+          getData()
         }}
       />
-      <Text style={styles.uploadedTitle}>Uploaded Images:</Text>
+      <Text style={styles.uploadedTitle}>Products:</Text>
       {data &&
         data.map((obj, index) => (
           <View key={obj._id}>
             <Image
-              source={{ uri: 'http://192.168.1.111:5000/' + obj.productImage }}
+              source={{ uri: `http://${ip}:5000/` + obj.productImage }}
               style={styles.uploadedImage}
             ></Image>
             <Text>{obj.productName}</Text>
@@ -94,5 +130,11 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     marginRight: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 })
---
+++ client/getIp.js
@@ -0,0 +1,1 @@
+export const ip = "10.0.0.160"
