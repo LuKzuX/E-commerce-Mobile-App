e01fd7b0c0de8b96c1098379d8acd0630fa53208
LuKz <kabombr@gmail.com>
2025-03-17 21:29:39 -0400

    a

--- client/app/screens/SigninScreen.jsx
+++ client/app/screens/SigninScreen.jsx
@@ -1,12 +1,31 @@
 import { useSignin } from '../../utils/useSignin'
-import { Text } from 'react-native'
+import { View, Text, TextInput, Button, } from 'react-native'
+import { useState } from 'react'
 
 export default function SigninScreen() {
-  const { signin } = useSignin()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await signin(username, password)
+  }
 
   return (
-    <View>
-      
+    <View >
+      <Text >Sign In</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Sign In" onPress={handleSubmit} />
     </View>
   )
 }
--- client/utils/useSignin.js
+++ client/utils/useSignin.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 import { useNavigation } from '@react-navigation/native'
 import { AuthContext } from '../context/authContext'
 import { useContext } from 'react'
-
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const useSignin = () => {
   const navigation = useNavigation()
   const {user, setUser} = useContext(AuthContext)
+  
   const signin = async (email, password) => {
     try {
       const res = await axios.post('/material-delivery/signin', {
