9680e758dc5f3d7d8d3a46638889cb5f4a0774b2
LuKz <kabombr@gmail.com>
2025-06-16 01:02:58 -0400

    a

--- server/utils/cloudflareConfig.js
+++ server/utils/cloudflareConfig.js
@@ -3,9 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const endpoint = `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`;
+console.log('R2 Endpoint:', endpoint);
+
 const s3Client = new S3Client({
   region: 'auto',
-  endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: endpoint,
   credentials: {
     accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID,
     secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY,
