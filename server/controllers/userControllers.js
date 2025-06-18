7b4c84fa390009cbac196e68603cde06686a4fc2
LuKz <kabombr@gmail.com>
2025-06-17 22:38:22 -0400

    a

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -109,7 +109,7 @@ export const updateUserInfo = async (req, res, next) => {
       const salt = bcrypt.genSaltSync(10)
       const hashedOldPassword = bcrypt.hashSync(currentUser.password, salt)
       const hashedCurrentPassword = bcrypt.hashSync(password, salt)
-      if (!bcrypt.compareSync(hashedOldPassword, hashedCurrentPassword)) {
+      if (!bcrypt.compareSync(password, currentUser.password)) {
         return res
           .status(400)
           .json({ statusText: 'Current Password is incorrect' })
